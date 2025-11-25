131ab3db8bbb0081370f3be800e98d401ef5b60a
kernaltrap <kernaltrap@proton.me>
2025-11-24 20:31:15 -0600

    [2025/11/24-20:31:00] Automated push
    Signed-off-by: kernaltrap <kernaltrap@proton.me>

--- autostart.sh
+++ autostart.sh
@@ -2,29 +2,29 @@
 # Autostart script for i3
 
 function spawn() {
-  if ! [ pgrep -x "$1" ]; then
+  bname=$(basename "$1")
+  if ! pgrep -f -x "$bname"; then
     "$@" & disown
   fi
 }
 
 # Shell components
-spawn env XDG_CURRENT_DESKTOP=i3 \
-      XDG_SESSION_TYPE=x11 \
-      XDG_SESSION_DESKTOP=i3 \
-      dbus-update-activation-environment DISPLAY XDG_CURRENT_DESKTOP
+dbus-update-activation-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 spawn wlr-randr --output HDMI-A-1 --custom-mode 1920x1080@100.000000Hz
-export QT_QPA_PLATFORMTHEME=qt5ct
-export QT_QPA_PLATFORMTHEME=qt6ct
-export GTK_THEME=Adwaita:dark
-export GTK_APPLICATION_PREFER_DARK=1
-export vblank_mode=1
+spawn /usr/libexec/xdg-desktop-portal-wlr -r
 spawn mako --background-color "#000000" --border-color "#000000" --output DP-3
-spawn "$HOME/.config/i3/waybar.sh"
+spawn gentoo-pipewire-launcher restart
+# spawn "$HOME/.config/i3/waybar.sh"
 # spawn "$HOME/.config/wayfire/scripts/start-replay.sh"
 spawn gsettings set org.gnome.desktop.interface gtk-theme Adwaita:dark
 # spawn python3 "$HOME/.config/wayfire/scripts/ipc-scripts/firefox-pip-sticky.py"
-spawn swaybg -i "$HOME/.config/i3/wallpapers/galaxy.png"
+spawn swaybg -i $HOME/.config/i3/wallpapers/galaxy.png
 spawn "$HOME/.config/i3status/media.sh"
+spawn swayidle -w \
+         timeout 300 'if ! playerctl --all-players status 2>/dev/null | grep -q "^Playing$"; then swaylock -f -c 000000; fi' \
+         timeout 600 'if ! playerctl --all-players status 2>/dev/null | grep -q "^Playing$"; then swaymsg "output * dpms off"; fi' \
+          resume 'swaymsg "output * dpms on"'
+
 # User apps
 spawn zen
 spawn "$HOME/.local/share/Discord/Discord"
@@ -32,7 +32,7 @@ spawn keepassxc
 spawn SFP_UI
 spawn "$HOME/.config/wayfire/scripts/musicplayer-startup.sh"
 spawn protonvpn-app
-spawn steam
+#spawn steam # weird startup behavior... spawns a bunch of chromium windows. will keep commented until fix'd
 spawn dino
 
 exit
